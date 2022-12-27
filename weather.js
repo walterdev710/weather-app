564782a45aeebacb9f52b43ab7955b6cda6fc272
Walter:) <92471071+Genius-frontend-developer@users.noreply.github.com>
2022-12-27 16:46:03 +0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function getWeather(lat, lon, timezone){
-    return axios.get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&timeformat=unixtime", 
+    return axios.get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=mph&timeformat=unixtime", 
         { 
             params: {
                 latitude:lat,
